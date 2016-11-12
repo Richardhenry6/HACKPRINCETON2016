34b5345630ac33153cf4dc00e6dba3db8fe3ed97
James Poindexter <jaxazam@gmail.com>
2016-11-12 04:26:42 -0500

    new files: Card.java and Match.java

---
+++ Card.java
@@ -0,0 +1,13 @@
+public class Card {
+int id;
+String host_Name;
+String guest_Name;
+int host_PID;
+int guest_PID;
+int host_SSN;
+int guest_SSN;
+Meal meal; // I don’t think this is necessary... if it is, this is wrong
+Date date; // (Date.java)
+//Image picture; <- what class is this?
+
+}

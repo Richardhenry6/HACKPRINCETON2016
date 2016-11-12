10f75f2c8da6b98a4b1aefc89203b8fbafb7010f
Richard Henry <rh11@courselab02.cs.princeton.edu>
2016-11-12 05:11:13 -0500

    Match skeleton

--- Match.java
+++ Match.java
@@ -1,7 +1,47 @@
 public class Match {
 
-boolean match;
-Card host_card;
-Card guest_card;
-int id;
+    private boolean match;
+    private Card host_card;
+    private Card guest_card;
+    private int id;
+
+
+    public Match(Card host_card, Card guest_card, boolean match, int id)
+    { 
+	this.match = match;
+	this.host_card = host_card; 
+	this.guest_card = guest_card; 
+	this.id = id; 
+    }
+    
+    public Card get_host_card()
+    { 
+	return host_card; 
+    }
+    
+    public Card get_guest_card()
+    { 
+	return guest_card; 
+    }
+    
+    public int get_id() 
+    { 
+	return id; 
+    }
+    
+    public void set_host_card()
+    { 
+	this.host_card = host_card; 
+    }
+    
+    public void set_guest_card()
+    { 
+	this.guest_card = guest_card;
+    }
+    
+    public void set_id(int id) 
+    { 
+	this.id = id; 
+    }     
+    
 }

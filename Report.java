289249c2f7e649db06e977be59f43fe63974cf45
James Poindexter <jaxazam@gmail.com>
2016-11-12 06:18:14 -0500

    God damn it Isaac.... updated Report.java

--- Report.java
+++ Report.java
@@ -6,34 +6,34 @@ public class Report{
     {
         return;
     }
-    public static void bigfatgirls(Queue<Card> q)
+    public static void printToFile(Queue<Card> q)
     {
         try
         {
             PrintWriter print = new PrintWriter("Unmatched.txt", "UTF-8");
-            int len = q.size(); 
+            int len = q.size();
             for(int i = 0; i < len; i++)
             {
-                print.println(under_fucking_score(q.dequeue()));
+                print.println(printSetup(q.dequeue()));
             }
             print.close();
-            
-            
+
+
         } catch (Exception e) {
-   
+
         }
-    
+
     }
-    private static String under_fucking_score(Card card)
+    private static String printSetup(Card card)
     {
-        String ret = "ID: " + card.get_id() + " Host: " + card.get_host_Name() + 
+        String ret = "ID: " + card.get_id() + " Host: " + card.get_host_Name() +
             " Guest: " + card.get_guest_Name() + " Host PID: " + card.get_host_PID()
             + " Guest PID: " + card.get_guest_PID() + " Host SSN: " + card.get_host_SSN()
-            + " Guest Host SSN: "+ card.get_guest_SSN() + " Date: " + card.get_date() + 
+            + " Guest Host SSN: "+ card.get_guest_SSN() + " Date: " + card.get_date() +
             " Meal Type: " + card.get_meal();
         return(ret);
     }
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Queue<Card> q = new Queue<Card>();
         Date date = new Date(1,2,2016);
@@ -42,7 +42,7 @@ public class Report{
         Card card1 = new Card(2,2,"host","g",26,24,22,44,'c', date1);
         q.enqueue(card);
         q.enqueue(card1);
-        bigfatgirls(q);
-        
+        printToFile(q);
+
     }
 }

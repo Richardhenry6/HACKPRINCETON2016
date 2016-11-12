9a64bca293dee963fba92cacf01c757f2879bc42
luis <ctangel14@gmail.com>
2016-11-12 04:31:38 -0500

    uploading scanning.java

---
+++ Scanning.java
@@ -0,0 +1,15 @@
+/*
+ *
+ *
+ */
+
+public class Scanning {
+    public static void main (String[] args) {
+        // Scan Image or upload image
+        // Convert image into digital text - > confidence factor
+        // recognize 'label' 
+        // put infor into card class using id
+        // put cards into database
+        System.out.print("hey");
+    }
+}

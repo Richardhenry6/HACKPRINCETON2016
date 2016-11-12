4ba6adc5c29f94cda9eda16b830c5f742ebaf487
James Poindexter <jaxazam@gmail.com>
2016-11-12 05:24:25 -0500

    Card.java: 3 constructors, accessors, mutators, and compareTo() by ID

--- Card.java
+++ Card.java
@@ -1,13 +1,91 @@
 public class Card {
-int id;
-String host_Name;
-String guest_Name;
-int host_PID;
-int guest_PID;
-int host_SSN;
-int guest_SSN;
-Meal meal; // I don’t think this is necessary... if it is, this is wrong
-Date date; // (Date.java)
+private int id;  //the meal card ID (located in upper right corner of image)
+private int type; //-1 if a guest card; 1 if a host card
+private String host_Name; //host name (the one )
+private String guest_Name;
+private int host_PID;
+private int guest_PID;
+private int host_SSN;
+private int guest_SSN;
+private Meal meal; // I don’t think this is necessary... if it is, this is wrong
+private Date date; // (Date.java)
 //Image picture; <- what class is this?
 
+//Default Constructor
+public Card() {
+
+    int id = 0;
+    int type = 0;
+    String host_Name = NULL;
+    String guest_Name = NULL;
+    int host_PID = 0;
+    int guest_PID = 0;
+    int host_SSN = 0;
+    int guest_SSN 0;
+    Meal meal = NULL;
+    Date date = NULL;
 }
+
+//Complete Constructors
+//for all ints
+public Card(int id, int type, int host_PID, int guest_PID, int host_SSN, int guest_SSN){
+
+    this.id = id;
+    this.type = type;
+    this.host_PID = host_PID;
+    this.guest_PID = guest_PID
+    this.host_SSN = host_SSN;
+    this.guest_SSN = guest_SSN;
+}
+
+//Strings, Meal, and Date constructor
+public Card(String host_Name, String guest_Name, Meal meal, Date date) {
+    this.host_Name = host_Name;
+    this.guest_Name = guest_Name;
+    this.meal = meal;
+    this.date = date;
+}
+
+//Accessors
+public int get_id() { return id; }
+public int get_type(){ return type; }
+public int get_host_PID(){ return host_PID; }
+public int get_guest_PID(){ return guest_PID; }
+public int get_host_SSN(){ return host_SSN; }
+public int get_guest_SSN(){ return guest_SSN; }
+public String get_host_Name(){ return host_Name; }
+public String get_guest_Name(){ return guest_Name; }
+public Date get_date(){ return date; }
+public Meal get_meal(){ return meal; }
+
+//Mutators
+public int set_id(int id) { this.id = id; }
+public int set_type(int type) { this.type = type; }
+public int set_host_PID(int pid ) { host_PID = pid; }
+public int set_guest_PID(int pid ) { guest_PID = pid; }
+public int set_host_SSN(int num) { host_SSN = num; }
+public int set_guest_SSN(int num) { guest_SSN = num; }
+public String set_host_Name(String name) { host_Name = name; }
+public String set_guest_Name(String name) { guest_Name = name; }
+public Date set_date(Date date) { this.date = date; }
+public Meal set_meal(Meal meal) { this.meal = meal; }
+}
+
+//Sorting
+/*Compares this card to the specified date (work in progress...)
+
+public boolean equals(Other other) {
+    if (other == this) return true;
+    if (other == null) return false;
+    if (other.getClass() != this.getClass()) return false;
+    Card that = (Card) other;
+    return (this.id == that.id);
+}
+*/
+
+//based off of ID
+public int compareTo(Date that) {
+        if (this.id  < that.id)  return -1;
+        if (this.id  > that.id)  return +1;
+        return 0;
+    }
